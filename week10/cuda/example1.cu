ea67d6369579d2184c91ed6cb3eefdf81a691e14
Pedro Perez <pperezm@itesm.mx>
2017-10-20 10:03:54 -0500

    week10

---
+++ week10/cuda/example1.cu
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+__global__ void myKernel(void) {
+	printf("GPU: Hello world!\n");
+}
+
+
+int main() {
+	myKernel<<<1, 1>>>();
+	cudaDeviceSynchronize();
+	printf("Ending...\n");
+	return 0;
+}
