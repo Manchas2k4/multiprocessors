91976f81f456e605e404679fca90bf8080804aee
Pedro Perez <pperezm@itesm.mx>
2018-08-27 16:56:33 -0500

    java added

--- java_thread/MainExample6.java
+++ java_thread/MainExample6.java
@@ -5,48 +5,4 @@ import java.math.BigInteger;
 public class MainExample6 {
 	private static final int NUM = 100_000;
 	private static final int MAXTHREADS = Runtime.getRuntime().availableProcessors();
-	
-	public static void main(String args[]) {
-		Example6 threads[];
-		int block;
-		long startTime, stopTime;
-		double acum = 0;
-		BigInteger result = BigInteger.valueOf(1);
-		
-		block = NUM / MAXTHREADS;
-		threads = new Example6[MAXTHREADS];
-		
-		acum = 0;
-		for (int j = 1; j <= Utils.N; j++) {
-			for (int i = 0; i < threads.length; i++) {
-				if (i != threads.length - 1) {
-					threads[i] = new Example6((i + 1) * block, (i + 2) * block);
-				} else {
-					threads[i] = new Example6((i + 1) * block, NUM);
-				}
-			}
-			
-			startTime = System.currentTimeMillis();
-			for (int i = 0; i < threads.length; i++) {
-				threads[i].start();
-			}
-			for (int i = 0; i < threads.length; i++) {
-				try {
-					threads[i].join();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			stopTime = System.currentTimeMillis();
-			acum +=  (stopTime - startTime);
-		}
-		
-		result = BigInteger.valueOf(1);
-		for (int i = 0; i < threads.length; i++) {
-			result = result.multiply(threads[i].getResult());
-		}
-				
-		System.out.println("factorial(" + NUM + ") = " + result);
-		System.out.printf("avg time = %.5f\n", (acum / Utils.N));
-	}
 }
