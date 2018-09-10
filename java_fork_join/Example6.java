624c99b410cd636c98516d404c3931f555723ca1
Pedro Perez <pperezm@itesm.mx>
2018-09-10 14:09:02 -0500

    fork-join ended

--- java_fork_join/Example6.java
+++ java_fork_join/Example6.java
@@ -7,4 +7,30 @@ public class Example6 extends RecursiveTask<BigInteger> {
 	private static final long MIN = 10_000;
 	private int start, end;
 	
+	public Example6(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+	
+	protected BigInteger computeDirectly() {
+		BigInteger result = BigInteger.valueOf(1);
+		for (int i = start; i <= end; i++) {
+			result = result.multiply(BigInteger.valueOf(i));
+		}
+		return result;
+	}
+	
+	@Override
+	protected BigInteger compute() {
+		if ( (this.end - this.start) <= Example6.MIN ) {
+			return this.computeDirectly();
+			
+		} else {
+			int mid = (end + start) / 2;
+			Example6 lowerMid = new Example6(start, mid);
+			lowerMid.fork();
+			Example6 upperMid = new Example6(mid + 1, end);
+			return upperMid.compute().multiply(lowerMid.join());
+		}
+	}
 }
--- java_fork_join/MainExample6.java
+++ java_fork_join/MainExample6.java
@@ -7,5 +7,23 @@ public class MainExample6 {
 	private static final int MAXTHREADS = Runtime.getRuntime().availableProcessors();
 	
 	public static void main(String args[]) {
+		ForkJoinPool pool; 
+		long startTime, stopTime;
+		double acum = 0;
+		BigInteger result = BigInteger.valueOf(1);
+		
+		acum = 0;
+		for (int j = 1; j <= Utils.N; j++) {
+			startTime = System.currentTimeMillis();
+			
+			pool = new ForkJoinPool(MAXTHREADS);
+			result = pool.invoke(new Example6(1, NUM));
+			
+			stopTime = System.currentTimeMillis();
+			acum +=  (stopTime - startTime);
+		}
+		
+		System.out.println("factorial(" + NUM + ") = " + result);
+		System.out.printf("avg time = %.5f\n", (acum / Utils.N));
 	}
 }
