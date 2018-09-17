6d80dffd27da88afdfbbf18a7f08166b6a74de35
Pedro Perez <pperezm@itesm.mx>
2018-09-17 17:58:02 -0500

    openmp

--- openmp/intro5.c
+++ openmp/intro5.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <omp.h>
 
-#define N 7
+#define N 20
 
 int main(int argc, char* argv[]) {
 	int i;
 	
-	#pragma omp parallel private(i)
-	{
+	#pragma omp parallel for
+	for (i = 0; i < N; i++) {
 		int id = omp_get_thread_num();
-		for (i = 0; i < N; i++) {
-			printf("Thread ID %i Iteration %i\n", id, i);
-		}
+		printf("Thread ID %i Iteration %i\n", id, i);
 	}
 	return 0;
 }
