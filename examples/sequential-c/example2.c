771374b38ce37d0059666061a2e9f732b3fc0ccc
Pedro Perez <pperezm@tec.mx>
2019-08-23 11:49:50 -0500

    changes make to the example order

--- examples/sequential-c/example2.c
+++ examples/sequential-c/example2.c
@@ -1,51 +1,32 @@
+/* This code calculates an IP approximation */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "utils/cheader.h"
 
-#define SIZE 100000
-
-void counting_sort(int *array, int size, int copy) {
-	int *temp = (int*) malloc(sizeof(int) * size);
-	int i, j, count;
-	
-	for (i = 0; i < size; i++) {
-		count = 0;
-		for (j = 0; j < size; j++) {
-			if (array[j] < array[i]) {
-				count++;
-			} else if (array[i] == array[j] && j < i) {
-				count++;
-			}
-		}
-		temp[count] = array[i];
-	}
-	
-	if (copy) {
-		memcpy(array, temp, sizeof(int) * size);
-	}
-	free(temp);	
-}
+#define NUM_RECTS 1e9
 
 int main(int argc, char* argv[]) {
-	int i, *array;
-	double ms;
-	
-	array = (int*) malloc(sizeof(int) * SIZE);
-	random_array(array, SIZE);
-	display_array("before", array);
+	double mid, height, width, area, ms;
+	double sum;
+	int i, j;
 	
 	printf("Starting...\n");
+	ms = 0;
 	for (i = 0; i < N; i++) {
 		start_timer();
 		
-		counting_sort(array, SIZE, (i == (N - 1)));
+		sum = 0;
+		width = 1.0 / (double) NUM_RECTS;
+		for (j = 0; j < NUM_RECTS; j++) {
+			mid = (j + 0.5) * width;
+			height = 4.0 / (1.0 + (mid * mid));
+			sum += height;
+		}
+		area = width * sum;
 		
 		ms += stop_timer();
 	}
-	
-	display_array("after", array);
+	printf("PI = %.15lf\n", area);
 	printf("avg time = %.5lf ms\n", (ms / N));
-	free(array);
 	return 0;
 }
