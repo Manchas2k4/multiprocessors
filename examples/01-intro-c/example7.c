e80d8fefd05795c1714f3ddf3d408d7cbc5563f2
Pedro Perez <pperezm@tec.mx>
2022-08-22 10:36:49 -0500

    example7.c updated

--- examples/01-intro-c/example7.c
+++ examples/01-intro-c/example7.c
@@ -18,22 +18,13 @@
 #include <stdlib.h>
 #include "utils.h"
 
-#define MAXIMUM 1000000 //1e6
+#define MAXIMUM 5000000 //5e6
 
 // implement your code
 
 int main(int argc, char* argv[]) {
 	int i, *a;
 	double ms;
-
-	a = (int *) malloc(sizeof(int) * (MAXIMUM + 1));
-	printf("At first, neither is a prime. We will display to TOP_VALUE:\n");
-	for (i = 2; i < TOP_VALUE; i++) {
-		if (a[i] == 0) {
-			printf("%i ", i);
-		}
-	}
-	printf("\n");
 
 	printf("Starting...\n");
 	ms = 0;
@@ -44,13 +35,7 @@ int main(int argc, char* argv[]) {
 
 		ms += stop_timer();
 	}
-	printf("Expanding the numbers that are prime to TOP_VALUE:\n");
-	for (i = 2; i < TOP_VALUE; i++) {
-		if (a[i] == 1) {
-			printf("%i ", i);
-		}
-	}
-	printf("\n");
+	printf("result = %i\n", result);
 	printf("avg time = %.5lf ms\n", (ms / N));
 
 	free(a);
